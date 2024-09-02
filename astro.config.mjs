297c34ec0d8d294e1b6ae64752edcea173d360df
Diego <diegoyon@outlook.com>
2024-09-01 22:35:40 -0600

    fix sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -24,121 +24,139 @@ export default defineConfig({
   //   },
   // },
   prefetch: true,
-  integrations: [tailwind(), sitemap({
-    i18n: {
-      defaultLocale: "es",
-      // All urls that don't contain `fr` after `https://crossfitxela.com/` will be treated as default locale, i.e. `en`
+  integrations: [
+    tailwind(),
+    sitemap({
+      i18n: {
+        defaultLocale: "es",
+        // All urls that don't contain `fr` after `https://crossfitxela.com/` will be treated as default locale, i.e. `en`
+        locales: {
+          es: "es",
+          // The `defaultLocale` value must present in `locales` keys
+          en: "en",
+        },
+      },
+    }),
+    starlight({
+      title: "ScrewFast Docs",
+      defaultLocale: "root",
+      // https://github.com/withastro/starlight/blob/main/packages/starlight/CHANGELOG.md
+      // If no Astro and Starlight i18n configurations are provided, the built-in default locale is used in Starlight and a matching Astro i18n configuration is generated/used.
+      // If only a Starlight i18n configuration is provided, an equivalent Astro i18n configuration is generated/used.
+      // If only an Astro i18n configuration is provided, the Starlight i18n configuration is updated to match it.
+      // If both an Astro and Starlight i18n configurations are provided, an error is thrown.
       locales: {
-        en: "es",
-        // The `defaultLocale` value must present in `locales` keys
-        en: "en"
-      }
-    }
-  }), starlight({
-    title: "ScrewFast Docs",
-    defaultLocale: "root",
-    // https://github.com/withastro/starlight/blob/main/packages/starlight/CHANGELOG.md
-    // If no Astro and Starlight i18n configurations are provided, the built-in default locale is used in Starlight and a matching Astro i18n configuration is generated/used.
-    // If only a Starlight i18n configuration is provided, an equivalent Astro i18n configuration is generated/used.
-    // If only an Astro i18n configuration is provided, the Starlight i18n configuration is updated to match it.
-    // If both an Astro and Starlight i18n configurations are provided, an error is thrown.
-    locales: {
-      root: {
-        label: "Español",
-        lang: "es"
+        root: {
+          label: "Español",
+          lang: "es",
+        },
+        de: {
+          label: "Deutsch",
+          lang: "de",
+        },
+        en: {
+          label: "English",
+          lang: "en",
+        },
+        fa: {
+          label: "Persian",
+          lang: "fa",
+          dir: "rtl",
+        },
+        fr: {
+          label: "Français",
+          lang: "fr",
+        },
+        ja: {
+          label: "日本語",
+          lang: "ja",
+        },
+        "zh-cn": {
+          label: "简体中文",
+          lang: "zh-CN",
+        },
       },
-      de: {
-        label: "Deutsch",
-        lang: "de"
+      // https://starlight.astro.build/guides/sidebar/
+      sidebar: [
+        {
+          label: "Quick Start Guides",
+          translations: {
+            de: "Schnellstartanleitungen",
+            es: "Guías de Inicio Rápido",
+            fa: "راهنمای شروع سریع",
+            fr: "Guides de Démarrage Rapide",
+            ja: "クイックスタートガイド",
+            "zh-cn": "快速入门指南",
+          },
+          autogenerate: {
+            directory: "guides",
+          },
+        },
+        {
+          label: "Tools & Equipment",
+          items: [
+            {
+              label: "Tool Guides",
+              link: "tools/tool-guides/",
+            },
+            {
+              label: "Equipment Care",
+              link: "tools/equipment-care/",
+            },
+          ],
+        },
+        {
+          label: "Construction Services",
+          autogenerate: {
+            directory: "construction",
+          },
+        },
+        {
+          label: "Advanced Topics",
+          autogenerate: {
+            directory: "advanced",
+          },
+        },
+      ],
+      social: {
+        github: "https://github.com/mearashadowfax/ScrewFast",
       },
-      en: {
-        label: "English",
-        lang: "en"
+      disable404Route: true,
+      customCss: ["./src/assets/styles/starlight.css"],
+      favicon: "/favicon.ico",
+      components: {
+        SiteTitle: "./src/components/ui/starlight/SiteTitle.astro",
+        Head: "./src/components/ui/starlight/Head.astro",
+        MobileMenuFooter:
+          "./src/components/ui/starlight/MobileMenuFooter.astro",
+        ThemeSelect: "./src/components/ui/starlight/ThemeSelect.astro",
       },
-      fa: {
-        label: "Persian",
-        lang: "fa",
-        dir: "rtl"
-      },
-      fr: {
-        label: "Français",
-        lang: "fr"
-      },
-      ja: {
-        label: "日本語",
-        lang: "ja"
-      },
-      "zh-cn": {
-        label: "简体中文",
-        lang: "zh-CN"
-      }
-    },
-    // https://starlight.astro.build/guides/sidebar/
-    sidebar: [{
-      label: "Quick Start Guides",
-      translations: {
-        de: "Schnellstartanleitungen",
-        es: "Guías de Inicio Rápido",
-        fa: "راهنمای شروع سریع",
-        fr: "Guides de Démarrage Rapide",
-        ja: "クイックスタートガイド",
-        "zh-cn": "快速入门指南"
-      },
-      autogenerate: {
-        directory: "guides"
-      }
-    }, {
-      label: "Tools & Equipment",
-      items: [{
-        label: "Tool Guides",
-        link: "tools/tool-guides/"
-      }, {
-        label: "Equipment Care",
-        link: "tools/equipment-care/"
-      }]
-    }, {
-      label: "Construction Services",
-      autogenerate: {
-        directory: "construction"
-      }
-    }, {
-      label: "Advanced Topics",
-      autogenerate: {
-        directory: "advanced"
-      }
-    }],
-    social: {
-      github: "https://github.com/mearashadowfax/ScrewFast"
-    },
-    disable404Route: true,
-    customCss: ["./src/assets/styles/starlight.css"],
-    favicon: "/favicon.ico",
-    components: {
-      SiteTitle: "./src/components/ui/starlight/SiteTitle.astro",
-      Head: "./src/components/ui/starlight/Head.astro",
-      MobileMenuFooter: "./src/components/ui/starlight/MobileMenuFooter.astro",
-      ThemeSelect: "./src/components/ui/starlight/ThemeSelect.astro"
-    },
-    head: [{
-      tag: "meta",
-      attrs: {
-        property: "og:image",
-        content: "https://crossfitxela.com" + "/social.webp"
-      }
-    }, {
-      tag: "meta",
-      attrs: {
-        property: "twitter:image",
-        content: "https://crossfitxela.com" + "/social.webp"
-      }
-    }]
-  }), compressor({
-    gzip: false,
-    brotli: true
-  }), partytown()],
+      head: [
+        {
+          tag: "meta",
+          attrs: {
+            property: "og:image",
+            content: "https://crossfitxela.com" + "/social.webp",
+          },
+        },
+        {
+          tag: "meta",
+          attrs: {
+            property: "twitter:image",
+            content: "https://crossfitxela.com" + "/social.webp",
+          },
+        },
+      ],
+    }),
+    compressor({
+      gzip: false,
+      brotli: true,
+    }),
+    partytown(),
+  ],
   output: "static",
   experimental: {
     clientPrerender: true,
-    directRenderScript: true
-  }
+    directRenderScript: true,
+  },
 });
